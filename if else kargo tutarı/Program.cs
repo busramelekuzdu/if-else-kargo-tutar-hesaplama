9c897357a5c06ce9c5ea2d248f463f1c3fb93414
Büşra Melek Uzdu <busramelekuzdu@gmail.com>
2023-07-17 11:52:33 +0300

    Proje dosyası ekle.

---
+++ if else kargo tutarı/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_kargo_tutarı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ödenecek tutar:");
+            double tutar = Convert.ToDouble(Console.ReadLine());
+            if (tutar >= 30)
+            {
+                Console.WriteLine("ödenecek tutar:"+tutar);
+            }
+            else if (tutar < 30)
+            {
+                tutar = tutar + 6;
+                Console.WriteLine("ödenecek tutar:" + tutar );
+            }
+            Console.ReadLine();
+        }
+    }
+}
